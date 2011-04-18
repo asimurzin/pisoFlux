2a5e430cc60a3bd87b3cbb975f829d42c2716c3c
Andrey Simurzin <asimurzin@gmail.com>
2011-04-18 15:19:12 +0400

    remove "-test" feature

--- pisoFlux/r1_6/__init__.py
+++ pisoFlux/r1_6/__init__.py
@@ -167,16 +167,11 @@ def main_standalone( argc, argv ):
 
 
 #--------------------------------------------------------------------------------------
-import sys, os
 from Foam import FOAM_REF_VERSION
 if FOAM_REF_VERSION( ">=", "010600" ):
    if __name__ == "__main__" :
+      import sys, os
       argv = sys.argv
-      if len( argv ) >1 and argv[ 1 ]=="-test":
-         argv = None
-         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'propogated', 'r1.6', 'incompressible', 'pisoFoam', 'les', 'pitzDaily' )
-         argv = [ __file__, "-case", test_dir ]
-         pass
       os._exit( main_standalone( len( argv ), argv ) )
       pass
    pass
